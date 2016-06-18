a99b8fcffc8a2d80c57d17d8064444bc7d5702be
hernnyy <jonhnick15@gmail.com>
2016-06-18 19:15:58 -0300

    separo mensajes sin token

--- minbo.py
+++ minbo.py
@@ -32,63 +32,7 @@ kernel.learn("aiml/sara/numeros.aiml")
 kernel.learn("aiml/sara/sexo.aiml")
 kernel.learn("aiml/sara/sara.aiml")
 
-text_messages = {
-    'bienvenido':
-        'Bienvenid@, entusiasta {name}!\n\n'
-        'Este grupo es para mantener a todos los miembros de la comunidad Minka-IT conectados.\n'
-        'No dudes en plantear tus inquietudes aquí o en algunos de los canales de comunicación de Minka-IT!\n'
-        'Esperamos que disfrutes de formar parte del grupo :)',
-
-    'info':
-        'Mi nombre es *minbo*,\n'
-        'Soy el bot que te brindará información acerca de la comunidad Minka-IT.\n'
-        'Estoy en etapa de desarrollo, no dudes en agregarme nuevas funcionalidades!\n'
-        'Mi código se encuentra en GitHub! repositorio temporal: https://github.com/jorgex9/minka-it.git',
-
-    'help_calc':
-        'Uso\n'
-        '/calc expr\n'
-        '\n'
-        'Operaciones disponibles, constantes y funciones\n'
-        '\n'
-        'Expression Ejemplo                                 Salida\n'
-        "+           2 + 2       4.0\n"
-        "-           3 - 1       2.0\n"
-        "*           2 * 3       6.0\n"
-        "/           5 / 2       2.5\n"
-        "%           5 % 2       1.0\n"
-        "^           5 ^ 2       25.0\n"
-        "PI          PI          3.141592653589793\n"
-        "E           E           2.718281828459045\n"
-        "sin(x)      sin(0)      0.0\n"
-        "cos(x)      cos(PI)     - 1.0\n"
-        "tan(x)      tan(0)      0.0\n"
-        "asin(x)     asin(0)     0.0\n"
-        "acos(x)     acos(-1)    3.141592653589793\n"
-        "atan(x)     atan(PI)    1.2626272556789118\n"
-        "log(x)      log(1)      0.0\n"
-        "abs(x)      abs(-1)     1.0\n"
-        "ceil(x)     ceil(2.7)   3.0\n"
-        "floor(x)    floor(2.7)  2.0\n"
-        "round(x)    round(2.7)  3.0\n"
-        "exp(x)      exp(2)      7.38905609893065\n",
-
-
-    'help_chat':
-        'Puedes hablar conmigo si estas aburrid@.\n'
-        'Soy un Chaterbot basado en la base de conocimiento de SARA.\n'
-        'Estoy implementado gracias a pyaiml.\n'
-        'Mi creador es la comunidad Minka-IT.\n'
-        'Proximamente mi creador me dotara de capacidad de aprendizaje en tiempo real.',
-
-    'help_wiki':
-        'Uso:\n'
-        '/wiki "Tu consulta"\n'
-        '/wikipedia "Tu consulta"\n',
-
-    'wrong_chat':
-        'Hubo un error'
-}
+import texts
 
 def listener(messages):
     for m in messages:
@@ -124,7 +68,7 @@ def on_user_joins(message):
 def on_info(message):
     chat_id = message.chat.id
     #bot.reply_to(message, text_messages['info']) # respondemos con el mensaje correspondiente a info que está en el diccionario
-    bot.send_message(chat_id, text_messages['info']) # respondemos con el mensaje correspondiente a info que está en el diccionario
+    bot.send_message(chat_id, texts.text_messages['info']) # respondemos con el mensaje correspondiente a info que está en el diccionario
 
 @bot.message_handler(commands=["ping"])
 def on_ping(message):
