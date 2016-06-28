2765fd76c0541b0358e10598c968bdb6b5c69ee1
hernnyy <jonhnick15@gmail.com>
2016-06-27 21:03:57 -0300

    bug de ejecucu¡ion de script

--- initMinbo.sh
+++ initMinbo.sh
@@ -1,8 +1,7 @@
-#!/bin/bash##
 # 1. Verifica si estan corriendo los procesos
 PROCESOMINBO=`ps aux | grep -c minbo`
 if [[ $PROCESOMINBO -eq 1 ]]
  then
-   phyton minbo.py &
+   python minbo.py &
 fi
 exit $1
