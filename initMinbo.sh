56b6fbb31ce329ab90213a2b8dd1c878d201dde7
hernnyy <jonhnick15@gmail.com>
2016-07-31 23:08:54 -0300

    mejora de script para cron

--- initMinbo.sh
+++ initMinbo.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
 # 1. Verifica si estan corriendo los procesos
-PROCESOMINBO=`ps aux | grep -c minbo`
+PROCESOMINBO=`ps aux | grep -c minbo.py`
+#echo $PROCESOMINBO
 if [[ $PROCESOMINBO -eq 1 ]]
  then
    python minbo.py &
+ else
+   DATE=`date +%Y-%m-%d:%H:%M:%S`
+   echo $DATE
+   echo $PROCESOMINBO
 fi
 exit $1
